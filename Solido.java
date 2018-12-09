15ea6ed494c517fdcd07482cfbaa4e0c2c48df0e
s4875747n <45730250+s4875747n@users.noreply.github.com>
2018-12-09 12:52:27 +0100

    Delete Solido.java

--- Solido.java
+++
@@ -1,9 +0,0 @@
-public abstract class Solido{ 
-    private double pesoSpecifico;
-    public Solido(double pesoSpecifico){
-      this.pesoSpecifico = pesoSpecifico;  
-    }
-    public abstract double volume(); 
-    public abstract double superficie();
-    public double peso(){return pesoSpecifico * volume(); } 
-} 
